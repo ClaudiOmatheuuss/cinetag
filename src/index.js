a827d54ba64bc85b6cc024dea914a4393bf0a040
Claud10matheus <claudiom.pst@gmail.com>
2023-09-14 11:58:05 -0300

    criando pagina inicial e instalando router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import AppRoutes from 'routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Olá Mundo</h1>
+    <AppRoutes />
   </React.StrictMode>
 );
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,7 @@
+function Inicio() {
+    return (
+        <h1>Olá mundo! Estou numa nova pagina uhul!!</h1>
+    )
+}
+
+export default Inicio;
