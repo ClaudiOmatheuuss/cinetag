1617a6158d6f5b87d06c439c8635a55a7697448c
Claud10matheus <claudiom.pst@gmail.com>
2023-09-14 15:12:00 -0300

    criando card

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import styles from './Card.module.css';
+import iconeFavoritar from './favoritar.png'
+
+function Card ({ id, titulo, capa }) {
+    return (
+        <div className={styles.container}>
+            <img src={capa} alt={titulo} className={styles.capa} />
+            <h2>{titulo}</h2>
+            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar} />
+        </div>
+    )
+}
+
+export default Card;
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,7 @@
 import Cabecalho from "components/Cabecalho";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 
 function Inicio() {
@@ -11,6 +12,7 @@ function Inicio() {
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <Card id='1' titulo='Gato curioso' capa='https://cdn2.thecatapi.com/images/xPuWATvp9.jpg' />
             <Rodape />
         </>
     )
