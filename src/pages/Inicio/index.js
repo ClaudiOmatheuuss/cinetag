821e2f3c8c3a06e825dcc246b8d83a213c813dfd
Claud10matheus <claudiom.pst@gmail.com>
2023-09-14 12:50:38 -0300

    resolvendo desafio do rodape

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,12 @@
 import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
 
 function Inicio() {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Rodape />
+        </>
     )
 }
 
