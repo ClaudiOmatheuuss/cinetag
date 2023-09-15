8acd8ff7910986d52802371313f2a4bc766e2799
Claud10matheus <claudiom.pst@gmail.com>
2023-09-14 21:59:43 -0300

    resolvendo desafio de criar pagina de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,21 @@
-import styles from './Favoritos.module.css'
+import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
+
+import styles from './Favoritos.module.css';
 
 function Favoritos () {
-
-
+    return (
+        <>
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id="1" titulo="Gato molhado" capa="https://cdn2.thecatapi.com/images/92p.jpg" />
+            </section>
+        </>
+    )
 }
 
-export default Favoritos
+export default Favoritos;
