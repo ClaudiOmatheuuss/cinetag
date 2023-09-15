bb198efa20fb442d53590461df420313c4130287
Claud10matheus <claudiom.pst@gmail.com>
2023-09-15 15:03:41 -0300

    usando conceito de rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,23 @@
-import Cabecalho from "components/Cabecalho";
-import Container from "components/Container";
-import Rodape from "components/Rodape";
-import FavoritosProvider from "contexts/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes () {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
+
                     <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                        <Route path="*" element={<NaoEncontrada />}></Route>
+                        <Route path="/" element={<PaginaBase />}>
+                            <Route index element={<Inicio />}></Route>
+                            <Route path="favoritos" element={<Favoritos />}></Route>
+                            <Route path=":id" element={<Player />}></Route>
+                            <Route path="*" element={<NaoEncontrada />}></Route>
+                        </Route>
                     </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+                    
         </BrowserRouter>
     )
 }
